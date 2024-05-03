c2b6193a4d59aefd927c65d6498a79dc84e07f35
camenduru <54370274+camenduru@users.noreply.github.com>
2024-05-03 06:43:05 +0300

    test

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from email.policy import default
 from json import encoder
 import gradio as gr
-import spaces
 import numpy as np
 import torch
 import requests
@@ -502,7 +501,6 @@ def change_visiale_by_model_type(_model_type):
 
 
 ######### Image Generation ##############
-@spaces.GPU(duration=120)
 def process_generation(_sd_type,_model_type,_upload_images, _num_steps,style_name, _Ip_Adapter_Strength ,_style_strength_ratio, guidance_scale, seed_,  sa32_, sa64_, id_length_,  general_prompt, negative_prompt,prompt_array,G_height,G_width,_comic_type):
     _model_type = "Photomaker" if _model_type == "Using Ref Images" else "original"
     if _model_type == "Photomaker" and "img" not in general_prompt:
